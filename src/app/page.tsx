f98098e2317d0c46c993db7a919ca16fd72eca4f
Art0citus <mritik424@gmail.com>
2026-07-06 01:51:45 +0530

    done with navbar and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import LoadingScreen from "@/components/LoadingScreen";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -15,6 +16,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
