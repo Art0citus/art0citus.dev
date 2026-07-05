55c69f5dfdc6831f45b782bf5202d9cb9ecb64cf
Art0citus <mritik424@gmail.com>
2026-07-05 23:44:31 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import LoadingScreen from "../../component/loadingscreen";
+"use client";
+
+import { useState } from "react";
+
+import LoadingScreen from "@/components/LoadingScreen";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <LoadingScreen />;
+  const [loading, setLoading] = useState(true);
+
+  return loading ? (
+    <LoadingScreen onComplete={() => setLoading(false)} />
+  ) : (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
