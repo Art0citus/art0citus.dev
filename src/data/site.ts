fc84493f22384004a281bc0deb74f723f8259b6e
Art0citus <mritik424@gmail.com>
2026-07-05 23:45:40 +0530

    added

---
+++ src/data/site.ts
@@ -0,0 +1,9 @@
+export const site = {
+  name: "Ritik Mishra",
+  title: "Backend Software Engineer",
+  email: "mritik424@gmail.com",
+  github: "...",
+  linkedin: "...",
+  substack: "...",
+  location: "Delhi, India",
+};
