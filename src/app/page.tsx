f33e7ef9b5a61633c5cba28825f00051946cb0d9
Art0citus <mritik424@gmail.com>
2026-07-08 23:21:16 +0530

    added stackscroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import LoadingScreen from "@/components/LoadingScreen";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -16,6 +17,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <Projects />
       <Footer />
     </>
   );
