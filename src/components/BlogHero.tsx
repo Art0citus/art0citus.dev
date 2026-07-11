68a94027d6ef7fb5d22da222908d4b7874e5c736
Art0citus <mritik424@gmail.com>
2026-07-12 01:17:17 +0530

    added sample

--- src/components/BlogHero.tsx
+++ src/components/BlogHero.tsx
@@ -5,20 +5,20 @@ import Image from "next/image";
 export default function BlogHero() {
   return (
     <section className="flex w-full justify-center px-4 pt-24">
-      <div className="relative w-full max-w-4xl overflow-hidden  border border-gray-200 shadow-lg">
+      <div className="relative w-full max-w-4xl overflow-hidden">
         {/* Background GIF */}
-        <div className="relative mt-24 h-[420px] overflow-hidden rounded-t-6xl">
+        <div className="relative mt-24 h-[250px] overflow-hidden rounded-t-6xl">
           <Image
             src="/gifs/cinema.gif"
             alt="Cinema"
             fill
             priority
             unoptimized
-            className="object-cover"
+            className="scale-110 object-cover blur-[2px]"
           />
 
           {/* Dark overlay for readability */}
-          <div className="absolute inset-0 bg-black/40" />
+          <div className="absolute inset-0 bg-black/50" />
 
           {/* Content */}
           <div className="absolute inset-37 flex flex-col justify-center p-8 text-white">
