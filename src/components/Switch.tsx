37d7167a60b98b9f942ac3054cf646812f3b641e
Art0citus <mritik424@gmail.com>
2026-08-18 03:58:15 +0530

    fix

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,33 +1,41 @@
 "use client";
 
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Switch = ({ isChecked, onChange }) => {
+interface SwitchProps {
+  isChecked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Switch = ({ isChecked, onChange }: SwitchProps) => {
   return (
     <StyledWrapper>
       <div className="toggle-container">
-        <input 
-          className="toggle-input" 
-          type="checkbox" 
+        <input
+          className="toggle-input"
+          type="checkbox"
           checked={isChecked}
           onChange={onChange}
         />
+
         <div className="toggle-handle-wrapper">
           <div className="toggle-handle">
             <div className="toggle-handle-knob" />
+
             <div className="toggle-handle-bar-wrapper">
               <div className="toggle-handle-bar" />
             </div>
           </div>
         </div>
+
         <div className="toggle-base">
           <div className="toggle-base-inside" />
         </div>
       </div>
     </StyledWrapper>
   );
-}
+};
 
 const StyledWrapper = styled.div`
   .toggle-container {
@@ -48,7 +56,7 @@ const StyledWrapper = styled.div`
     height: var(--knob-size);
     opacity: 0;
     font: inherit;
-    transition: transform .24s cubic-bezier(.65, 1.35, .5, 1);
+    transition: transform 0.24s cubic-bezier(0.65, 1.35, 0.5, 1);
     cursor: pointer;
   }
 
@@ -60,8 +68,16 @@ const StyledWrapper = styled.div`
     position: absolute;
     z-index: 1;
     bottom: -135%;
-    -webkit-mask-image: linear-gradient(to bottom, #000 62.125%, transparent 50%);
-    mask-image: linear-gradient(to bottom, #000 62.125%, transparent 50%);
+    -webkit-mask-image: linear-gradient(
+      to bottom,
+      #000 62.125%,
+      transparent 50%
+    );
+    mask-image: linear-gradient(
+      to bottom,
+      #000 62.125%,
+      transparent 50%
+    );
     width: 200%;
     overflow: hidden;
   }
@@ -72,10 +88,12 @@ const StyledWrapper = styled.div`
     align-items: center;
     transform: rotate(-25deg);
     transform-origin: bottom center;
-    transition: transform .24s cubic-bezier(.65, 1.35, .5, 1);
+    transition: transform 0.24s cubic-bezier(0.65, 1.35, 0.5, 1);
   }
 
-  .toggle-input:checked + .toggle-handle-wrapper > .toggle-handle {
+  .toggle-input:checked
+    + .toggle-handle-wrapper
+    > .toggle-handle {
     transform: rotate(25deg);
   }
 
@@ -85,37 +103,49 @@ const StyledWrapper = styled.div`
     border-radius: 50%;
     width: var(--knob-size);
     height: var(--knob-size);
-    background-image: radial-gradient(farthest-corner at 70% 30%, #ffffff 4%, #61bdda 12% 24%, #4791b2 50% 65%, #61bdda 75%);
-    transition: transform .24s cubic-bezier(.65, 1.35, .5, 1);
+    background-image: radial-gradient(
+      farthest-corner at 70% 30%,
+      #ffffff 4%,
+      #61bdda 12% 24%,
+      #4791b2 50% 65%,
+      #61bdda 75%
+    );
+    transition: transform 0.24s cubic-bezier(0.65, 1.35, 0.5, 1);
   }
 
-  .toggle-input:checked + .toggle-handle-wrapper .toggle-handle-knob {
+  .toggle-input:checked
+    + .toggle-handle-wrapper
+    .toggle-handle-knob {
     transform: rotate(-90deg);
   }
 
   .toggle-handle-knob::after {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
     border-radius: inherit;
     width: 100%;
     height: 100%;
-    box-shadow: inset 0 0 8px 2px rgb(255 255 255 / .4);
+    box-shadow: inset 0 0 8px 2px rgb(255 255 255 / 0.4);
     opacity: 0;
-    transition: opacity .2s;
+    transition: opacity 0.2s;
   }
 
   @media (hover: hover) {
-    .toggle-input:hover + .toggle-handle-wrapper .toggle-handle-knob::after,
-    .toggle-input:focus-visible + .toggle-handle-wrapper .toggle-handle-knob::after {
+    .toggle-input:hover
+      + .toggle-handle-wrapper
+      .toggle-handle-knob::after,
+    .toggle-input:focus-visible
+      + .toggle-handle-wrapper
+      .toggle-handle-knob::after {
       opacity: 1;
     }
   }
 
   .toggle-handle-bar-wrapper {
     position: relative;
-    width: .3em;
+    width: 0.3em;
     height: 1.2em;
   }
 
@@ -125,25 +155,36 @@ const StyledWrapper = styled.div`
     left: 0;
     width: 100%;
     height: calc(100% + var(--knob-size) / 2);
-    background-image: linear-gradient(to right, #1f1f1f, #161616, #1f1f1f 45% 55%, #161616, #1f1f1f);
-    background-position-x: .06125em;
-    transition: background-position-x .24s cubic-bezier(.65, 1.35, .5, 1);
-    box-shadow: inset 0 0.5em .15em rgb(0 0 0 / .4);
+    background-image: linear-gradient(
+      to right,
+      #1f1f1f,
+      #161616,
+      #1f1f1f 45% 55%,
+      #161616,
+      #1f1f1f
+    );
+    background-position-x: 0.06125em;
+    transition: background-position-x 0.24s
+      cubic-bezier(0.65, 1.35, 0.5, 1);
+    box-shadow: inset 0 0.5em 0.15em rgb(0 0 0 / 0.4);
   }
 
-  .toggle-input:checked + .toggle-handle-wrapper .toggle-handle-bar {
-    background-position-x: -.06125em;
+  .toggle-input:checked
+    + .toggle-handle-wrapper
+    .toggle-handle-bar {
+    background-position-x: -0.06125em;
   }
 
   .toggle-base {
     position: relative;
     border-radius: 3.125em;
-    padding: .15em;
+    padding: 0.15em;
     width: 2.2em;
     height: 0.7em;
     background-color: #222222;
     background-image: linear-gradient(to bottom, #444444, #222222);
-    box-shadow: 0 -.15em .3em #fff, 0 .15em .3em #d7d7d7;
+    box-shadow: 0 -0.15em 0.3em #fff,
+      0 0.15em 0.3em #d7d7d7;
   }
 
   .toggle-base-inside {
@@ -151,23 +192,36 @@ const StyledWrapper = styled.div`
     border-radius: inherit;
     width: 100%;
     height: 100%;
-    background-image: linear-gradient(to bottom, #1f1f1f, #0f0f0f);
-    box-shadow: inset 0 .0625em rgb(255 255 255 / .2), inset 0 -.03125em rgb(255 255 255 / 1), inset 0 -.0625em .15em rgb(0 0 0 / .1);
+    background-image: linear-gradient(
+      to bottom,
+      #1f1f1f,
+      #0f0f0f
+    );
+    box-shadow: inset 0 0.0625em rgb(255 255 255 / 0.2),
+      inset 0 -0.03125em rgb(255 255 255 / 1),
+      inset 0 -0.0625em 0.15em rgb(0 0 0 / 0.1);
   }
 
   .toggle-base-inside::after {
-    content: '';
+    content: "";
     position: absolute;
     border-radius: inherit;
     width: 100%;
     height: 100%;
-    background-image: linear-gradient(to bottom, #00cfb6, #00a5bf);
+    background-image: linear-gradient(
+      to bottom,
+      #00cfb6,
+      #00a5bf
+    );
     box-shadow: inherit;
     opacity: 0;
-    transition: opacity .24s cubic-bezier(.65, 1.35, .5, 1);
+    transition: opacity 0.24s
+      cubic-bezier(0.65, 1.35, 0.5, 1);
   }
 
-  .toggle-input:checked ~ .toggle-base .toggle-base-inside::after {
+  .toggle-input:checked
+    ~ .toggle-base
+    .toggle-base-inside::after {
     opacity: 1;
   }
 `;
